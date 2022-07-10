30aff81f49e54a65a88229ba3fd333725f78c979
Ivan Starčević <istarcevic16@gmail.com>
2022-07-10 18:01:31 +0200

    push notifications

--- src/app.js
+++ src/app.js
@@ -9,6 +9,15 @@ if ("serviceWorker" in navigator) {
     });
 }
 
+const notificationButton2 = document.getElementById("push-button");
+notificationButton2.addEventListener("click", () => {
+  Notification.requestPermission().then((result) => {
+    if (result === "granted") {
+      randomNotification();
+    }
+  });
+});
+
 const button = document.querySelector("#button");
 const input = document.querySelector("#input");
 const result = document.querySelector("#result");
@@ -53,6 +62,7 @@ async function shortenURL(e) {
 
 function writeData(url) {
   result.innerHTML = `<a target="_blank"href="${url}">${url}</a>`;
+  randomNotification();
 }
 
 function notificationPopUp() {
@@ -61,4 +71,38 @@ function notificationPopUp() {
   setTimeout(function () {
     x.className = x.className.replace("show", "");
   }, 3000);
+}
+
+function randomNotification() {
+  const notifTitle = ["Hey!", "Hello!", "Hi!"];
+  const notifBody = [
+    "Be sure you are connected to the internet for this app to work properly.",
+    "Sadly we are only able to handle 1 request per minute.",
+    "If you like this app please consider supporting us.",
+  ];
+  const notifIcon = ["🍔", "🍕", "🍗"];
+  const notifColor = ["#423EAF", "#FCBA06", "#ff0000"];
+  const notifImage = ["./img/logo512.png"];
+  const notifOptions = {
+    body:
+      notifIcon[Math.floor(Math.random() * notifIcon.length)] +
+      notifBody[Math.floor(Math.random() * notifBody.length)],
+    icon: notifImage[Math.floor(Math.random() * notifImage.length)],
+    image: notifImage,
+    vibrate: [100, 50, 100, 50, 100, 50, 100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: 1,
+    },
+    actions: [
+      { action: "explore", title: "Explore this site", icon: "🚀" },
+      { action: "close", title: "Close", icon: "✖" },
+    ],
+  };
+  new Notification(
+    notifTitle[Math.floor(Math.random() * notifTitle.length)],
+    notifOptions,
+    notifColor[Math.floor(Math.random() * notifColor.length)]
+  );
+  setTimeout(randomNotification, 10000);
 }
