b81af355c67f8624260ed48f9d3c522737bd206f
Ivan Starčević <istarcevic16@gmail.com>
2022-07-10 16:07:11 +0200

    newAPI

--- src/app.js
+++ src/app.js
@@ -12,6 +12,17 @@ if ("serviceWorker" in navigator) {
 const button = document.querySelector("#button");
 const input = document.querySelector("#input");
 const result = document.querySelector("#result");
+const encodedParams = new URLSearchParams();
+
+const options = {
+  method: "POST",
+  headers: {
+    "content-type": "application/x-www-form-urlencoded",
+    "X-RapidAPI-Key": "cec857fbcamsha3388521cc606f7p1d86c4jsn0f427a07db6d",
+    "X-RapidAPI-Host": "url-shortener-service.p.rapidapi.com",
+  },
+  body: encodedParams,
+};
 
 button.addEventListener("click", shortenURL);
 
@@ -20,19 +31,20 @@ async function shortenURL(e) {
   if (input.value === "") {
     notificationPopUp();
   } else {
+    encodedParams.append("url", `https://${input.value}`);
     result.innerHTML = `<span class="loader loader-active"></span>`;
     try {
       let data = await fetch(
-        `https://short-link-api.vercel.app/?query=${input.value}`,
-        {
-          cors: true,
-          origin: "*",
-          "Access-Control-Allow-Origin": "*",
-        }
+        "https://url-shortener-service.p.rapidapi.com/shorten",
+        options
       );
       data = await data.json();
-      let url = data["da.gd"];
+      let url = data.result_url;
+      console.log(url);
       writeData(url);
+      if (url === undefined) {
+        result.innerHTML = `<p style="text-align:center">We can only handle 1 request per minute,<br> please try again later.</p>`;
+      }
     } catch (error) {
       result.innerHTML = "ERROR. please try again.";
     }
